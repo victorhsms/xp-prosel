d3b533d282fa1b3ff640ef34503c1c3740ad573d
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-15 14:55:59 -0300

    test(login): teste de input de email

---
+++ src/__tests__/login.test.tsx
@@ -0,0 +1,15 @@
+import Login from '../pages/login'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+describe('Ao acessar a página de login', () => {
+  beforeEach(() => {
+    render(<Login />)
+  })
+
+  test('Deve existir um input de email', () => {
+    const emailInput = screen.queryByPlaceholderText('email@email.com')
+
+    expect(emailInput).toBeInTheDocument()
+  })
+})
