11933c1e87ce976c692c29f12657077216fd7aad
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-15 13:51:15 -0300

    refactor(global): adicionado recoilroot ao escopo global da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
