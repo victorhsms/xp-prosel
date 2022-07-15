c5777bdf63a035b6e487cc8f1ed45fede4d1c844
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-15 15:24:36 -0300

    feat(login): adicionado label de input

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,7 @@ export default function Login() {
   return (
     <form action="">
       <h1>Login</h1>
+      <label htmlFor="">Email</label>
       <input type="text" placeholder="email@email.com" />
     </form>
   )
