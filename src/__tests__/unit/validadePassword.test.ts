bec144abe0a6fdffaf1f1362f09a2885f4ba726a
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-15 17:01:19 -0300

    test(validatepassword): testes da função de validar senha

---
+++ src/__tests__/unit/validadePassword.test.ts
@@ -0,0 +1,22 @@
+import React from 'react'
+import validatePassword from '../../utils/validations/validatePassword'
+
+describe('A função deve retornar "true"', () => {
+  test('ao informar uma senha com 6 caracteres ou maior', () => {
+    const result = validatePassword('123456')
+
+    expect(result).toBeTruthy()
+  })
+})
+
+describe('a função deve retornar "false"', () => {
+  test('ao informar uma senha com menos de 6 caracteres', () => {
+    const result = validatePassword('123')
+    expect(result).toBeFalsy()
+  })
+
+  test('ao não informar uma senha', () => {
+    const result = validatePassword('')
+    expect(result).toBeFalsy()
+  })
+})
