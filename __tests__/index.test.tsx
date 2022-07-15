a4348bb8be90597feb703555ae4f4a989bf71d68
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-15 13:34:40 -0300

    chore(global): instalado o recoil

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import Home from '@/pages/index'
+import Home from 'src/pages/index'
 
 describe('Home', () => {
   it('renders a heading', () => {
