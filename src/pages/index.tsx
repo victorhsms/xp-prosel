27098a4c2895a2c631e0ab5bcf23a2278165bd92
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-15 14:20:27 -0300

    feat(index): redirecionamento sendo feito do index para o login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { GetServerSideProps } from 'next'
+
 export default function Home() {
   return (
     <div>
       <h1>Portfolio</h1>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: true
+    }
+  }
 }
