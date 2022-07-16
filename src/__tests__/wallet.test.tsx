719fef4ee2122dd91363f031cf47c889aa0ef7b6
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-16 18:55:16 -0300

    test(wallet): adicionado teste de modal para depósito

--- src/__tests__/wallet.test.tsx
+++ src/__tests__/wallet.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
 import { RecoilRoot } from 'recoil'
 import actions from '../mock/actions'
@@ -40,5 +40,34 @@ describe('Ao entrar na página home', () => {
     })
 
     expect(drawBalance).toBeInTheDocument()
+  })
+})
+
+describe('Ao clicar no botão "Fazer depósito"', () => {
+  beforeEach(() => {
+    const dateNow = new Date().toLocaleString()
+    localStorage.setItem(
+      'logged_user#xp-prosel',
+      JSON.stringify(['email@email.com', dateNow])
+    )
+    render(
+      <RecoilRoot>
+        <Home actions={actions} />
+      </RecoilRoot>
+    )
+  })
+
+  it('deve exibir um modal com o título "Depósito"', () => {
+    const deposit = screen.getByRole('button', {
+      name: /Fazer depósito/i
+    })
+
+    fireEvent.click(deposit)
+
+    const depositModal = screen.queryByRole('heading', {
+      name: /Depósito/i
+    })
+
+    expect(depositModal).toBeInTheDocument()
   })
 })
