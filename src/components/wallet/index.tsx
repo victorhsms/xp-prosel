31a0fd87e9b38c5c354d336bbd0a0511a5deae35
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-16 19:04:49 -0300

    chore(global): instalado dependencia de tipagem do react-modal

--- src/components/wallet/index.tsx
+++ src/components/wallet/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
+
 export default function Wallet() {
+  const [showModalDeposit, setShowModalDeposit] = useState<boolean>(false)
   return (
     <aside>
       <h3>Minha carteira</h3>
-      <button>Fazer depósito</button>
+      <button onClick={() => setShowModalDeposit(!showModalDeposit)}>
+        Fazer depósito
+      </button>
+
       <button>Retirar saldo</button>
     </aside>
   )
