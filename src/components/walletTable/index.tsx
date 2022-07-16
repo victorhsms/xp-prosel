3663b7541d85a333b8f0326c6dfceeed6a40fe2d
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-16 13:44:12 -0300

    feat(wallettable): adicionada linha única com tabela vazia

--- src/components/walletTable/index.tsx
+++ src/components/walletTable/index.tsx
@@ -1,3 +1,22 @@
 export default function WalletTable() {
-  return <h3>Minhas Ações</h3>
+  return (
+    <div>
+      <h3>Minhas Ações</h3>
+      <table data-testid="wallet-table">
+        <thead>
+          <tr>
+            <th>Ações</th>
+            <th>Quantidade</th>
+            <th>{'Valor(R$)'}</th>
+            <th>Negociar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr data-testid="wallet-table-empty">
+            <td>Você não tem nenhuma ação comprada no momento!</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
