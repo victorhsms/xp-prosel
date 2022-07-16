1ec9d0fa634a739225915c402a9828d6a9cc4f98
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-15 22:54:15 -0300

    feat(header): clicar em logoff leva para /login

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import md5 from 'crypto-js/md5'
 import { useUser } from '../../state/hooks/useUser'
+import { useAddUser } from '../../state/hooks/useAddUser'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+  const router = useRouter()
   const user = useUser()
+  const addUser = useAddUser()
+
   function getGravatar() {
     const hashGenerated = md5(user).toString()
-    const gravatar = `https://www.gravatar.com/avatar/${hashGenerated}`
-    return gravatar
+    return `https://www.gravatar.com/avatar/${hashGenerated}`
+  }
+
+  function logoff() {
+    localStorage.removeItem('logged_user#xp-prosel')
+    addUser('')
+
+    router.replace('/login')
   }
 
   return (
     <header>
       <div>
-        <span role="link">logoff</span>
+        <span role="link" onClick={logoff}>
+          logoff
+        </span>
         <img src={getGravatar()} alt="Foto de perfil" />
       </div>
     </header>
