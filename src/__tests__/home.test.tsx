2b91d870acae6c009eeae78f90e02cb913befc38
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-21 08:47:32 -0300

    test(home): adicionado teste para getServersideprops do nextjs

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,10 +1,12 @@
 import { render, renderHook } from '@testing-library/react'
 import React from 'react'
 import { RecoilRoot } from 'recoil'
-import Home from '../pages'
+import Home, { getServerSideProps } from '../pages'
 import { useRouter } from 'next/router'
 import mockRouter from 'next-router-mock'
 import actions from '../mock/actions'
+import { NextParsedUrlQuery } from 'next/dist/server/request-meta'
+import { GetServerSidePropsContext } from 'next'
 
 jest.mock('next/router', () => require('next-router-mock'))
 
@@ -24,5 +26,36 @@ describe('Caso não tenha usuário logado', () => {
     })
 
     expect(result.current).toMatchObject({ asPath: '/login' })
+  })
+})
+
+describe('Ao ser chamada getServerSideProps na Home', () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          status: 'success',
+          message: actions
+        })
+    })
+  ) as jest.Mock
+
+  it('Deve ser retornado um array de ações nas props', async () => {
+    const context = {
+      params: { id: '' } as NextParsedUrlQuery
+    }
+    const response = await getServerSideProps(
+      context as GetServerSidePropsContext
+    )
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          actions: {
+            status: 'success',
+            message: actions
+          }
+        }
+      })
+    )
   })
 })
