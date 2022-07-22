f28f6c214847f8d7cdb4cabc45b9714508f9c61f
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-22 09:40:03 -0300

    fix(recoilhook): resolvido bug em que ações iguais compradas eram duplicadas

---
+++ src/helpers/reorganizeActions.ts
@@ -0,0 +1,19 @@
+import IActions from 'src/interface/action'
+
+export default function reorganizeAction(
+  actionsInState: IActions[],
+  actionBuyed: IActions[],
+  repeatedAction: IActions
+) {
+  return actionsInState.map(action => {
+    if (action.name === repeatedAction.name) {
+      const newAction = {
+        name: repeatedAction.name,
+        quantity: actionBuyed[0].quantity + repeatedAction.quantity,
+        value: repeatedAction.value
+      }
+      return newAction
+    }
+    return action
+  })
+}
--- src/state/hooks/useAddActionWallet.ts
+++ src/state/hooks/useAddActionWallet.ts
@@ -1,11 +1,25 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil'
+import reorganizeAction from '../../helpers/reorganizeActions'
 import IActions from '../../interface/action'
 import { actionsWallet } from '../atom'
 
 export const useAddActionWallet = () => {
   const setActions = useSetRecoilState(actionsWallet)
   const actionsInState = useRecoilValue(actionsWallet)
-  return (actions: IActions[]) => {
-    return setActions([...actionsInState, ...actions])
+  return (actionBuyed: IActions[]) => {
+    const repeatedAction = actionsInState.find(
+      action => action.name === actionBuyed[0].name
+    )
+
+    if (repeatedAction) {
+      const newActions = reorganizeAction(
+        actionsInState,
+        actionBuyed,
+        repeatedAction
+      )
+      return setActions(newActions)
+    }
+
+    return setActions([...actionsInState, ...actionBuyed])
   }
 }
