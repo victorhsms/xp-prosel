7f384b8267c191e1c870916a722133a74645d1be
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-22 23:17:25 -0300

    feat(global): adicionados resets e padrões de estilo global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+import { GlobalStyles } from '../styles/global'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <GlobalStyles />
       <Component {...pageProps} />
     </RecoilRoot>
   )
---
+++ src/styles/global.ts
@@ -0,0 +1,135 @@
+import { createGlobalStyle } from 'styled-components'
+
+// Workaroud for https://github.com/styled-components/vscode-styled-components/issues/175
+const styled = { createGlobalStyle }
+
+export const GlobalStyles = styled.createGlobalStyle`
+  // custom
+
+  @media (max-width: 1080px) {
+    body.scrollock {
+      height: 100%;
+      overflow: hidden;
+    }
+  }
+  /* Box sizing rules */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* Global font family */
+  * {
+    font-family: 'Noto Sans', sans-serif;
+  }
+
+  /* Remove default margin and padding */
+  body,
+  h1,
+  ul,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd {
+    padding: 0;
+    margin: 0;
+  }
+
+  /* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+  ul[role='list'],
+  ol[role='list'] {
+    list-style: none;
+  }
+
+  /* Set core root defaults */
+  html:focus-within {
+    scroll-behavior: smooth;
+  }
+
+  /* Set core body defaults */
+  body {
+    min-height: 100vh;
+    text-rendering: optimizeSpeed;
+    line-height: 1.5;
+  }
+
+  /* Allow percentage-based heights in the application */
+  html,
+  body {
+    height: 100%;
+  }
+
+  /* Add accessible line-height and improve text rendering */
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  /* A elements that don't have a class get default styles */
+  a:not([class]) {
+    text-decoration-skip-ink: auto;
+    color: inherit;
+    text-decoration: none;
+  }
+
+  /* Make media tags easier to work with */
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    max-width: 100%;
+    display: block;
+  }
+
+  /* 8. Avoid text overflows */
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  /* Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  /* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+  @media (prefers-reduced-motion: reduce) {
+    html:focus-within {
+      scroll-behavior: auto;
+    }
+
+    *,
+    *::before,
+    *::after {
+      animation-duration: 0.01ms !important;
+      animation-iteration-count: 1 !important;
+      transition-duration: 0.01ms !important;
+      scroll-behavior: auto !important;
+    }
+  }
+
+  /* Create a root stacking context */
+  #root,
+  #__next {
+    isolation: isolate;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+  }
+`
