3f9fa84dd5ccbf1e364a1a11eb2b0c2af8a6f308
Victor Hugo <victorhsmsantos@gmail.com>
2022-07-24 08:27:19 -0300

    refactor(mock): retirada quantidade excessiva de ações

--- src/mock/actions.ts
+++ src/mock/actions.ts
@@ -6,20 +6,13 @@ const actions = [
   { name: 'iFood', quantity: 1, value: 280 },
   { name: 'Facebook', quantity: 1, value: 420 },
   { name: 'Google', quantity: 1, value: 310 },
-  { name: 'Nubank', quantity: 1, value: 340 },
-  { name: 'Uber', quantity: 1, value: 440 },
   { name: 'Oracle', quantity: 1, value: 130 },
   { name: 'Samsung', quantity: 1, value: 180 },
   { name: 'Xaiomi', quantity: 1, value: 200 },
   { name: 'Apple', quantity: 1, value: 260 },
   { name: 'Trybe', quantity: 1, value: 360 },
-  { name: 'MCDonalds', quantity: 1, value: 90 },
   { name: 'Burguer King', quantity: 1, value: 80 },
-  { name: 'Amazon', quantity: 1, value: 440 },
-  { name: 'Microsoft', quantity: 1, value: 415 },
   { name: 'Sony', quantity: 1, value: 280 },
-  { name: 'Acer', quantity: 1, value: 320 },
-  { name: 'Asus', quantity: 1, value: 330 },
   { name: 'HP', quantity: 1, value: 100 }
 ]
 
